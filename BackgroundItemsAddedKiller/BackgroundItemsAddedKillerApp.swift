314969b26a122c1424989691a4b241827e30a0a1
xiaogd <xiaogdgenuine@gmail.com>
2023-02-19 11:05:33 +0800

    Initial Commit

---
+++ BackgroundItemsAddedKiller/BackgroundItemsAddedKillerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackgroundItemsAddedKillerApp.swift
+//  BackgroundItemsAddedKiller
+//
+//  Created by xiaogd on 2023/2/19.
+//
+
+import SwiftUI
+
+@main
+struct BackgroundItemsAddedKillerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
